554993c366922420f45e97363191082732a24add
Seb <sxb1845@student.bham.ac.uk>
2024-12-11 14:02:18 +0000

    Further organisation of files

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder = "../frontend/templates", static_folder = "../frontend/static")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///csrs.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy()
-db.init_app(app)
 
-from routes.module_routes import module_routes
-app.register_blueprint(module_routes)
+@app.route('/')
+def home():
+    return {"message": "Welcome to the prototype backend!"}
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
