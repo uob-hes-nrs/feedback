ce17db3da8cef1f8a13605533b25da17ac08e0eb
rohan5046 <43728094+rohan5046@users.noreply.github.com>
2024-12-12 06:49:38 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Load the .env file if it exists
+source .env
+
+# Set up Python environment
 python3 -m venv .venv
+.venv/bin/pip install --upgrade pip
 .venv/bin/pip install -r requirements.txt --upgrade > /dev/null
+
+# Activate virtual environment
 source .venv/bin/activate
+
+# Run the application
 .venv/bin/python3 app.py
