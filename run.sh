86dee3cb39b50184b11cdbc85ee1aba5c2132c4b
Seb <sxb1845@student.bham.ac.uk>
2024-12-12 11:56:23 +0000

    Fix scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
 python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt --upgrade > /dev/null
 source .venv/bin/activate
-.venv/bin/python3 app.py
+pip install -r requirements.txt --upgrade > /dev/null
+python3 app.py
